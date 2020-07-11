ad837284dd96fdbf972455c7287e96e79625bbee
Husni Kamal <husnikamal3@gmail.com>
2020-07-11 07:20:31 +0700

    day 9 (factorial) & day 10 (six checker)

--- Main.java
+++ Main.java
@@ -1,51 +1,75 @@
 import java.util.*;
 
-
 public class Main {
 
+  private static Scanner in;
+
   public static void main(String[] args) {
-    System.out.println("Day 1 : Odd Number");
-    oddNumber();
+    // Scanner Init
+    initScanner();
+
+    // println("Day 1 : Odd Number");
+    // oddNumber();
+
+    // nextLine();
+
+    // println("Day 4 : Pyramid");
+    // pyramid();
+
+    // nextLine();
+
+    // println("Day 5 : Countdown");
+    // countDown();
+
+    // nextLine();
+
+    // println("Day 6 : Exponent");
+    // exponent();
+
+    // println("Day 7 : Factorial");
+    // factorial();
+
+    println("Day 9 : Six Checker");
+    sixChecker();
   }
 
   private static int amountOfNumber = 0;
 
   private static void oddNumber() {
-    Scanner in = new Scanner(System.in);
-
     try {
-      System.out.println("Enter amount of numbers");
+      println("Enter amount of numbers");
       amountOfNumber = in.nextInt();
 
       int[] arrOfNumbers = new int[amountOfNumber];
 
       for (int i = 0; i < amountOfNumber; i++) {
         int position = i + 1;
-        System.out.println("Enter number #" + position);
+        println("Enter number #" + position);
 
         arrOfNumbers[i] = in.nextInt();
       }
 
       for (int num : arrOfNumbers) {
         if (num % 2 != 0) {
-          System.out.print(num + " ");
+          print(num + " ");
         }
       }
 
-      System.out.println();
-      System.out.println();
+      nextLine();
+      nextLine();
 
-      System.out.println("Day 2 : Odd Number Count");
+      println("Day 2 : Odd Number Count");
       countOddNumber(arrOfNumbers);
 
-      System.out.println();
+      nextLine();
 
-      System.out.println("Day 3 : Sum Odd Number");
+      println("Day 3 : Sum Odd Number");
       sumOddNumber(arrOfNumbers);
 
-    } catch(Exception e) {
+      nextLine();
+    } catch (Exception e) {
       e.printStackTrace();
-      System.out.println("Please input number!");
+      println("Please input number!");
     }
   }
 
@@ -57,7 +81,7 @@ public class Main {
         totalOddNumber += 1;
       }
     }
-    System.out.println("Total Odd Number : " + totalOddNumber);
+    println("Total Odd Number : " + totalOddNumber);
   }
 
   private static void sumOddNumber(int[] arrOfNumbers) {
@@ -67,30 +91,147 @@ public class Main {
         sumTotalOddNumber += num;
       }
     }
-    System.out.println("Sum Total Odd Number : " + sumTotalOddNumber);
-
-    pyramid();
-
+    println("Sum Total Odd Number : " + sumTotalOddNumber);
   }
 
   private static void pyramid() {
-    Scanner in = new Scanner(System.in);
-    
-    System.out.println();
-    System.out.println("Enter total rows : ");
-
     try {
-      System.out.println("Day 4 : Pyramid");
+      println("Enter total rows : ");
       int row = in.nextInt();
 
       for (int i = 1; i <= row; i++) {
         for (int j = 0; j < i; j++) {
-          System.out.print("*");
+          print("*");
         }
-        System.out.println();
+        nextLine();
       }
-    } catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private static void countDown() {
+    nextLine();
+    println("Enter limit : ");
+
+    try {
+      int limit = in.nextInt();
+      if (limit < 2) {
+        println("Limit minimum is 2!");
+        countDown();
+        return;
+      }
+
+      for (int i = limit; i > 0; i--) {
+        print(i + " ");
+      }
+
+      nextLine();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void exponent() {
+    try {
+      println("Enter number :");
+      int number = in.nextInt();
+
+      println("Enter exponent :");
+      int exponent = in.nextInt();
+
+      int total = 1;
+      for (int i = 0; i < exponent; i++) {
+        total = total * number;
+      }
+
+      println("Result : " + total);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void factorial() {
+    try {
+      println("Enter number :");
+      int number = in.nextInt();
+
+      int result = 1;
+
+      if (number > 0) {
+        for (int i = number; i > 0; i--) {
+          result = result * i;
+        }
+      } else if (number < 0) {
+        println("Undefined. Factorial cannot be negative number!");
+        return;
+      }
+
+      println("Factorial result : " + result);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void sixChecker() {
+    try {
+      println("Enter number count : ");
+      int numberCount = in.nextInt();
+
+      if (numberCount <= 0) {
+        println("Count number must not lower than 0 or zero!");
+        sixChecker();
+        return;
+      }
+
+      List<Integer> arrOfIntegers = new ArrayList<>();
+
+      for (int i = 1; i <= numberCount; i++) {
+        println("Enter " + ordinalSuffix(i) + " number : ");
+        arrOfIntegers.add(in.nextInt());
+      }
+
+      if (arrOfIntegers.get(0) == 6 || arrOfIntegers.get(numberCount - 1) == 6) {
+        println("Gotcha!!");
+      } else {
+        print("Not Gotcha!!");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static String ordinalSuffix(int num) {
+    String numStr = "" + num;
+
+    if (String.valueOf(numStr.charAt(numStr.length() - 1)) == "1") {
+      return num + "st";
+    }
+
+    if (String.valueOf(numStr.charAt(numStr.length() - 1)) == "2") {
+      return num + "nd";
+    }
+
+    if (String.valueOf(numStr.charAt(numStr.length() - 1)) == "3") {
+      return num + "rd";
+    }
+
+    return num + "th";
+  }
+
+  private static void initScanner() {
+    in = new Scanner(System.in);
+  }
+
+  private static void print(String message) {
+    System.out.print(message);
+  }
+
+  private static void println(String message) {
+    System.out.println(message);
+  }
+
+  private static void nextLine() {
+    System.out.println();
   }
 }
